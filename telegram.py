83521ac07f8f3f62cce0d7942daaf30648daf6e0
ILIA <gurilvik1@yandex.ru>
2023-02-14 21:23:32 +0300

    лад

--- telegram.py
+++ telegram.py
@@ -1,23 +1,18 @@
-# d1={'a' : 1}
-# d2={'s' : 2}
-# d3 = d1 | d2
-# print(d3)
+# print({'a' : 1} | {'s' : 2})
 
 # python -m pip install telebot
-# Далее создается файл секретный для получения токена - получить токен на bot father
-# на нем получается новый бот 
-# token="194435528:AAEy1X9NkzpW7Ugc0gbs8DFCax-RVNjsLd4"
+
 from secret import token
 import telebot
 bot = telebot.TeleBot(token)
 
-@bot.message_handler(commands= ['start'])
-def start(message):
-    print ("У меня начался рабочий день!")
-    bot.send_message(
-        message.chat.id,
-        '<b>Я Вас слушаю)</b>',
-        parse_mode='html')
+# @bot.message_handler(commands= ['start'])
+# def start(message):
+#     print ("У меня начался рабочий день!")
+#     bot.send_message(
+#         message.chat.id,
+#         '<b>Я Вас слушаю)</b>',
+#         parse_mode='html') - это уже сам код работы бота
 
 
 bot.polling(none_stop=True)
